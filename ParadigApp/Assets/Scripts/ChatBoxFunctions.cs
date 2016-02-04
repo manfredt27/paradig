d36a30b0fa89653e4161bd2c16258d950c52b426
Joaquin Mena <jmena@pernix-solutions.com>
2016-02-04 16:15:18 -0600

    message prefab updated

--- ParadigApp/Assets/Scripts/ChatBoxFunctions.cs
+++ ParadigApp/Assets/Scripts/ChatBoxFunctions.cs
@@ -7,12 +7,17 @@ public class ChatBoxFunctions : MonoBehaviour {
 
 	//WebService webService;
 	string message = "";
-	[SerializeField] Transform messageParentPanel;
-	[SerializeField] GameObject newMessagePrefab;
-	[SerializeField] GameObject newFutureMessagePrefab;
+	[SerializeField] Transform messageParentPanel; // scroll
+
+	[SerializeField] GameObject UserParentPanelPrefab; // user prefab
+	[SerializeField] GameObject FutureParentPanelPrefab; // future prefab
+
+	[SerializeField] Text futureTime; // future time
+	[SerializeField] Text userTime; // user time
+
 	List<GameObject> messageList = new List<GameObject>();
 	bool isEmptyList = true;
-	public bool isUserMessage = false;
+	bool isUserMessage = false;
 	private int questionId = 0;
 	AssemblyCSharp.Questions question = new AssemblyCSharp.Questions ();
 
@@ -33,7 +38,6 @@ public class ChatBoxFunctions : MonoBehaviour {
 
 	void Update()
 	{
-		Debug.Log (messageList[messageList.Count - 1]);
 	}
 
 	public void SetMessage (string pMesssage){
@@ -51,14 +55,18 @@ public class ChatBoxFunctions : MonoBehaviour {
 			GameObject clone;
 			if (this.isUserMessage)
 			{
-				clone = (GameObject)Instantiate (newMessagePrefab); // new Object 
+				clone = (GameObject)Instantiate (UserParentPanelPrefab); // new Object 
 			}
 			else
 			{
-				clone = (GameObject)Instantiate (newFutureMessagePrefab); // new Object 
-			} 
+				clone = (GameObject)Instantiate (FutureParentPanelPrefab); // new Object 
+
+			}
+			this.futureTime.text = System.DateTime.UtcNow.ToString ("HH:mm");
+			this.userTime.text = System.DateTime.UtcNow.ToString ("HH:mm");
 			clone.transform.SetParent (messageParentPanel); // set new object to parent object
 			clone.transform.SetSiblingIndex (messageParentPanel.childCount - 2);
+
 			clone.GetComponent<MessageFunctions> ().ShowMessage (this.message); // set message to new object to show in GUI
 			if (this.isEmptyList)
 			{
@@ -68,7 +76,7 @@ public class ChatBoxFunctions : MonoBehaviour {
 				} 
 				else
 				{
-					clone.transform.localPosition = new Vector3 (-145, 450, 0);				
+					clone.transform.localPosition = new Vector3 (0, 450, 0);				
 				}
 				this.isEmptyList = false;
 			}
@@ -77,12 +85,12 @@ public class ChatBoxFunctions : MonoBehaviour {
 				Vector3 vector = messageList [messageList.Count - 1].transform.localPosition;
 				if (isUserMessage)
 				{
-					clone.transform.localPosition = new Vector3 (240, vector.y - 90, 0);
+					clone.transform.localPosition = new Vector3 (180, vector.y - 90, 0);
 					this.isUserMessage = false;
 				}
 				else
 				{
-					clone.transform.localPosition = new Vector3 (195, vector.y - 90, 0);
+					clone.transform.localPosition = new Vector3 (0, vector.y - 90, 0);
 					this.isUserMessage = true;
 				}	
 			}
--- ParadigApp/Assets/Scripts/MessageFunctions.cs
+++ ParadigApp/Assets/Scripts/MessageFunctions.cs
@@ -4,9 +4,20 @@ using UnityEngine.UI;
 
 public class MessageFunctions : MonoBehaviour {
 
-	[SerializeField] Text text;
+	[SerializeField] GameObject FutureParentPanelPrefab; // future prefab
+	[SerializeField] GameObject UserParentPanelPrefab; // future prefab
 
 	public void ShowMessage(string pMessage){
-		text.text = pMessage;
+		if (FutureParentPanelPrefab != null) 
+		{
+			// get text from panel
+			FutureParentPanelPrefab.transform.GetChild (0).GetComponent<Text> ().text = pMessage;
+		}
+		if (UserParentPanelPrefab) 
+		{
+			// get text from panel
+			UserParentPanelPrefab.transform.GetChild (0).GetComponent<Text> ().text = pMessage;
+		}
+
 	}
 }
